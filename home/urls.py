036aec9f6e696c2f4baf50c3c8693396e5ae53a2
beahnjoka <74968625+beahnjoka@users.noreply.github.com>
2024-03-01 13:45:00 +0300

    team

--- home/urls.py
+++ home/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
     path('partners/', views.PartnerList.as_view()),
     path('partners/<int:pk>/', views.ListPartnersById.as_view()),
 
-  # team routes
-    path('team/', views.TeamList.as_view()),
-    path('team/<int:pk>/', views.ListTeamById.as_view()),
+#   # team routes
+#     path('team/', views.TeamList.as_view()),
+#     path('team/<int:pk>/', views.ListTeamById.as_view()),
 
     # testimonial routes
     path('testimonials/', views.TestimonialList.as_view()),

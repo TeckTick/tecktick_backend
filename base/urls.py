455943ef9988f6b93b1b9250eab2e0f109079f64
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2024-03-10 09:28:20 +0300

    update readme

--- base/urls.py
+++ base/urls.py
@@ -44,6 +44,7 @@ urlpatterns = [
     path('api/home/', include('home.urls')),
     path('api/blog/', include('blog.urls')),
    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
   

c8a8ddb1787d0254dc616396a9fcf40fb1ce60a4
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2024-03-17 18:21:56 +0300

    config for hosting

--- base/settings.py
+++ base/settings.py
@@ -109,7 +109,7 @@ DATABASES = {
 }
 
 
-# Password validation
+# Password validation 
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
